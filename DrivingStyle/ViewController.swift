d77398f7394b01a85489e52ce3da8fc9a18138ba
ivannesquick <neskin8@gmail.com>
2021-01-05 00:34:59 +0700

    Initial Commit

---
+++ DrivingStyle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DrivingStyle
+//
+//  Created by Neskin Ivan on 05.01.2021.
+//  Copyright © 2021 Neskin Ivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
